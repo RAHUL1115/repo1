a9186322aa9407e326d73bf1971a1b3cc343916c
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2022-11-04 12:44:41 +0530

    pre relese

--- publish/index.js
+++ publish/index.js
@@ -1,10 +1,9 @@
-// const { program } = require('commander');
 const fs = require('fs-extra');
 const path = require('path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-// ? repo1 path and repo1 object path
+// * repo1 path and repo1 object path
 const REPO1 = path.join(path.resolve())
 const REPO1OBJECT = path.join(REPO1,'/object/repo1.json')
 const REPO2 = path.join(path.resolve(),'./../repo2')
@@ -13,18 +12,8 @@ const REPO3 = path.join(path.resolve(),'./../repo3')
 const REPO3PACKAGE = path.join(REPO3,'package.json')
 
 
-// // * pre prcoess arguments function
-// function processArgs(){
-//     program.option('-k1, --key1 <int>')
-//     program.option('-k2, --key2 <int>');
-//     program.parse();
-//     const options = program.opts();
-//     return options;
-// }
-
 // * main function
 async function main(){
-    // const options = processArgs();
     try {
         // * declearation
         let allRepos = []
@@ -43,14 +32,10 @@ async function main(){
         // * wirte the output to file
         await fs.writeJSON(REPO1OBJECT,allRepoVersion)
 
-        // let command = `git push origin master`;
         let command = `cd ${REPO1} && git commit -am 'pre relese' && npm version patch && git push origin master && npm publish`;
         const { stdout, stderr } = await exec(command)
-        // if(stderr){
-        //     throw new Error(stderr)
-        // }
-        console.log('1',stdout)
-        console.log('2',stderr)
+        console.log('stdout : ',stdout)
+        console.log('stderr : ',stderr)
     } catch (error) {
         throw new Error(error)
     }
