2e3466e44124c3fbcead6de48736c79b23b1df83
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2022-11-07 14:48:42 +0530

    major changes

---
+++ publish.js
@@ -0,0 +1,42 @@
+const fs = require('fs-extra');
+const path = require('path');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+// * repo1 path and repo1 object path
+const TARCKERREPOURL = 'https://github.com/rahul1115/repo1.git';
+const TRACKERFOLDER = './.tracker/'
+const PARENTPACKAGEJSONPATH = path.join(path.resolve(), './package.json');
+const TRACKERPATH = path.join(path.resolve(),TRACKERFOLDER);
+const TRACKERJSONTPATH = path.join(TRACKERPATH,'./object/repo.json');
+
+// * main function
+async function main(){
+    try {
+        // get the version of current repository and name
+        let name, version;
+        let parentPackageJSON = await fs.readJSON(PARENTPACKAGEJSONPATH)
+        name = parentPackageJSON.name;
+        version = parentPackageJSON.version;
+
+        
+        // update the verison number in the object file
+        let trackerJSON = await fs.readJSON(TRACKERJSONTPATH);
+        trackerJSON['name'] = version;
+        await fs.writeJSON(TRACKERJSONTPATH,trackerJSON)
+        console.log(trackerJSON)
+
+        // // run npm update of tracker repository
+        // let command2 = `cd ${TRACKERREPOPATH} && git commit -am 'pre relese' && npm version patch && git push origin master && npm publish`;
+        // const stdData2 = await exec(command2)
+        // console.log(`stdout : ${stdData2.stdout}`)
+        // console.log(`stderr : ${stdData2.stderr}`)
+
+        // post clean up
+        await fs.remove(TRACKERPATH);
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+main()
