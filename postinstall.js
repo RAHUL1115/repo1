d46315072101d15b41d7dc1124ab86d0fcfb21e6
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2022-12-16 14:54:43 +0530

    pre-relese

--- postinstall.js
+++ postinstall.js
@@ -1,15 +1,13 @@
-// require repo1
-const fs = require('fs')  
+const fs = require('fs-extra');
+const path = require('node:path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const tracker = require('./index'); // get tracker object
 
-const tracker = require('./index') // get tracker object
-const parentPackageJSON = require('../../../package.json') // get parent packageJSON
-
-// else always install prod
-(async function() { 
-    let prodPackages = parentPackageJSON?.nexsales?.prod || {};
-    let devPackages = parentPackageJSON?.nexsales?.dev || {};
+async function main() { 
+    const parentPackageJSON = fs.readJSONSync(path.join(path.resolve(), 'package.json'));
+    let prodPackages = parentPackageJSON?.nexsales?.prod;
+    let devPackages = parentPackageJSON?.nexsales?.dev;
 
     // necessary variables
     let packagesToInstall = ``;
@@ -20,9 +18,9 @@ const parentPackageJSON = require('../../../package.json') // get parent package
         packageObject = devPackages;
     }
 
-    // check if packageObject is type of object
-    if(typeof(a) != 'object'){
-        packageObject = {};
+    // check if packageObject is valid JSON object
+    if(typeof(packageObject) != 'object' || packageObject == {}) {
+        return;
     }
 
     // construct packages string from packageObject
@@ -36,5 +34,8 @@ const parentPackageJSON = require('../../../package.json') // get parent package
     // ? run the install command
     let command = `npm install --no-save ${packagesToInstall}`;
     const stdData = await exec(command)
-    console.log(`stdout : ${stdData.stdout}`)
-})()
+    console.log(stdData.stdout)
+    console.log(stdData.stderr);
+}
+
+main()
